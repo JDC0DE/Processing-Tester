9248d925c3aaf1cb9b9a550ac4d9a6717c41cb3f
JDC0DE <54870450+JDC0DE@users.noreply.github.com>
2022-08-05 01:57:35 +1000

    Project Files

---
+++ Project/PTest/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        if (args[0].equals("-pt")) {
+            try {
+
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        }
+
+    }
+}
