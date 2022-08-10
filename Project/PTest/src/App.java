aece77004b56cf9b26805fe5ea4dbb1e26300c93
JDC0DE <54870450+JDC0DE@users.noreply.github.com>
2022-08-11 00:43:01 +1000

    Printing a Converted Processing file to text file
    Using a converted processing file the contents of the file are printed into a text file.

--- Project/PTest/src/App.java
+++ Project/PTest/src/App.java
@@ -1,12 +1,49 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        if (args[0].equals("-pt")) {
-            try {
+import java.io.*;
 
-            } catch (Exception e) {
-                // TODO: handle exception
+public class App {
+    private static String parameterTest = "-pt";
+    private static String functionalityTest = "-ft";
+    private static String outPut = "OUTPUT";
+    private static String str = "";
+    private static String txt = "";
+
+    public static void main(String[] args) throws IOException {
+        File javaFile = new File(
+                "C:\\Users\\joshu\\OneDrive\\Documents\\Uni\\COMP4050\\Processing-Tester\\sketch_220803a.java");
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader bf = new BufferedReader(in);
+        try {
+            if (args[0].equals(parameterTest)) {
+                while (!str.equals("EXIT")) {
+                    str = bf.readLine();
+                    if (str.equals("OUTPUT")) {
+                        txtOutput(javaFile);
+                    }
+                }
+                System.out.println("System: exiting now!");
+                System.exit(0);
+
+            }
+        } catch (Exception e) {
+            System.out.println("Error: invalid input parameters");
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void txtOutput(File input) throws IOException {
+        PrintWriter out = new PrintWriter("output.txt");
+        BufferedReader jfReader = new BufferedReader(new FileReader(input));
+        System.out.println(jfReader.readLine());
+        while (jfReader.readLine() != null) {
+            txt = jfReader.readLine();
+            out.println(txt);
+            if (txt == null) {
+                break;
             }
         }
+        out.close();
+        jfReader.close();
 
     }
 }
