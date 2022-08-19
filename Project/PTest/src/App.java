3efbafcc8b2d20cdf14ce47ab1fd17688157566f
JDC0DE <54870450+JDC0DE@users.noreply.github.com>
2022-08-19 15:58:21 +1000

    Updated compile method + checks for inputs
    Using scanner instead of buffered reader -better for text reading + regex checks for folder, files

--- Project/PTest/src/App.java
+++ Project/PTest/src/App.java
@@ -1,9 +1,10 @@
 import java.io.*;
+import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-
+import java.util.regex.*;
 import javax.lang.model.SourceVersion;
 import javax.tools.*;
 
@@ -13,30 +14,57 @@ public class App {
     private static String dynamicTest = "-dt";
     private static String outPut = "-o";
     private static String exit = "-e";
+    private static String help = "-h";
     private static String str = "";
     private static String txt = "";
 
     // "C:\\Users\\joshu\\OneDrive\\Documents\\Uni\\COMP4050\\Processing-Tester\\sketch_220803a.java"
+    // "C:\\Users\\joshu\\OneDrive\\Documents\\Uni\\COMP4050\\Processing-Tester\\sketch_220803a.pde"
+    // "C:\\Users\\joshu\\OneDrive\\Documents\\Uni\\COMP4050\\Processing-Tester\\SampleClass.java"
+    // \w+\.java - matches any alpha numeric character + .java
+    // \w+\.pde - matches any alpha numeric character + .pde
     public static void main(String[] args) throws IOException {
         // arguments need to account for folder naming convention before checking
         // contents, file type checked is java or naming convention of java file
+        // need to check for correct folder/file name convnetions -
+        // Submissions/SID_FN_LN/Project Name/Files
+        // name of test file? -
+        // Will the program also loop through the submissions folder and check each
+        // students subfolder or will it only take a single project submission at a
+        // time and then finish?
         InputStreamReader in = new InputStreamReader(System.in);
         BufferedReader bf = new BufferedReader(in);
         try {
-            File javaFile = new File(args[0]);
+            File inputFile = new File(args[0]);
+            System.out.println(args[0]);
+
+            if (inputFile.isDirectory() && inputFile.exists()) {
+                System.out.println("its a folder! - tonmoys recursive algo");
+            }
+
+            if (inputFile.isFile() && inputFile.exists()) {
+                if (Pattern.matches("\\w+\\.java", inputFile.getName())) {
+                    System.out.println("is java valid match: " + inputFile.getName());
+                }
+
+                if (Pattern.matches("\\w+\\.pde", inputFile.getName())) {
+                    System.out.println("is pde valid match: " + inputFile.getName());
+                }
+
+            }
 
             if (args[0].length() > 0) {
                 while (!str.equals(exit)) {
                     str = bf.readLine();
                     if (str.equals(compileTest)) {
-                        compile(javaFile);
+                        compile(inputFile);
                     }
                     if (str.equals(staticTest)) {
                         System.out.println("Static Test Check");
                     }
                     if (str.equals(outPut)) {
-                        txtOutput(javaFile);
-                        bf.close();
+                        txtOutput(inputFile);
+                        // bf.close();
                     }
                 }
                 System.out.println("System: exiting now!");
@@ -55,17 +83,17 @@ public class App {
 
     public static void txtOutput(File input) throws IOException {
         PrintWriter out = new PrintWriter("output.txt");
-        BufferedReader jfReader = new BufferedReader(new FileReader(input));
-        System.out.println(jfReader.readLine());
-        while (jfReader.readLine() != null) {
-            txt = jfReader.readLine();
+        Scanner fReader = new Scanner(new FileReader(input));
+        while (fReader.hasNextLine()) {
+            txt = fReader.nextLine();
+            // System.out.println(txt);
             out.println(txt);
             if (txt == null) {
                 break;
             }
         }
         out.close();
-        jfReader.close();
+        fReader.close();
 
     }
 
